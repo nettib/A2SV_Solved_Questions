f9a359a3fcd8da5e594e400903ad048c049b8e6d
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-04 15:09:17 +0300

    Add swap_case function to change letter cases

---
+++ sWAP cASE.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    ans = ""
+    
+    for char in s:
+        if char == char.lower():
+            ans += char.upper()
+        else:
+            ans += char.lower()
+    return ans
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
