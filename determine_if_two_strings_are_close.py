8bdeaf797dac6a2c2f1e8df4dc0a7a51925924a1
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-11 15:34:29 +0300

    Implement function to check if two strings are close

---
+++ determine_if_two_strings_are_close.py
@@ -0,0 +1,24 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        word1_hash = {}
+        word2_hash = {}
+
+        for char in word1:
+            word1_hash[char] = word1_hash.get(char, 0) + 1
+        
+        for char in word2:
+            word2_hash[char] = word2_hash.get(char, 0) + 1
+
+        if set(word1_hash.keys()) != set(word2_hash.keys()):
+            return False
+        
+        if sorted(word1_hash.values()) != sorted(word2_hash.values()):
+            return False
+
+        return True
+       
+
+        
+        
