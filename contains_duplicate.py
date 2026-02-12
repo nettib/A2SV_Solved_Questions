b73d01baf591a09a60db6cb6e72135f7827c7729
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-12 10:01:03 +0300

    Implement containsDuplicate method to check duplicates

---
+++ contains_duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        track = set()
+
+        for num in nums:
+            if num in track:
+                return True
+            track.add(num)
+        return False
+            
