29676d10b8a62bf278031f64d112f451abbc62f4
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-20 19:42:03 +0300

    Add diagonal traversal function to Solution class
    Implement diagonal traversal of a matrix.

---
+++ diagonal_traverse.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        track = {}
+        ans = []
+
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
+                if r + c not in track:
+                    track[r + c] = []
+                track[r + c].append(mat[r][c])
+
+        flag = 1
+        for val in track.values():
+            if flag:
+                ans.extend(val[::-1])
+                flag = 0
+            else:
+                ans.extend(val)
+                flag = 1
+        
+        return ans
