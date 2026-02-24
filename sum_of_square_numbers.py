6c3af3a4aad1c079c8b87f61492cf9a38a958ad8
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-02-24 19:21:43 +0300

    Implement judgeSquareSum to check sum of squares

---
+++ sum_of_square_numbers.py
@@ -0,0 +1,14 @@
+import math
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l, r = 0, int(math.sqrt(c))
+
+        while l <= r:
+            if l ** 2 + r ** 2 == c:
+                return True
+            elif l ** 2 + r ** 2 < c:
+                l += 1
+            else:
+                 r -= 1
+        
+        return False
