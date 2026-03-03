42657ee671c95106d650176b8c6e091d08bf3507
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-04 01:29:54 +0300

    Create continuous_subarray_sum.py

---
+++ continuous_subarray_sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        track = {0: -1}
+
+        curr = 0
+
+        for i in range(len(nums)):
+            curr += nums[i]
+
+            rem = curr % k
+
+            if rem in track and i - track[rem] > 1:
+                return True
+            if rem not in  track:
+                track[rem] = i
+
+            
+        return False
