ac6d6085c0f923d6a63598c166871a7baa482e3b
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-06 19:24:37 +0300

    Add deleteNode method for linked list node deletion
    Implement deleteNode method to remove a node from a singly-linked list in-place.

---
+++ delete_node_in_a_linked_list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
+        curr = node
+        while curr.next.next:
+            curr.val = curr.next.val
+            curr = curr.next
+        curr.val = curr.next.val
+        curr.next = None
