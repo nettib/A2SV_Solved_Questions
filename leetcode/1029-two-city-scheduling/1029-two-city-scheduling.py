d0f10d6dcf37f4913017e2d9b6e7359fc4a202fb
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-03-17 11:11:50 +0300

    Add solution for Two City Scheduling

--- leetcode/1029-two-city-scheduling/1029-two-city-scheduling.py
+++ leetcode/1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -2,16 +2,19 @@ class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         diff = []
 
-        for i, [a_cost, b_cost] in enumerate(costs):
-            diff.append([b_cost - a_cost, i])
+        for i, cost in enumerate(costs):
+            a, b = cost
+            sep = b - a
+            diff.append([sep, i])
         
         diff.sort()
 
-        cost = 0
-        for i in range(len(diff) // 2):
-            cost += costs[diff[i][-1]][-1]
+        min_cost = 0
+
+        for i in range(len(diff)):
+            if i < (len(diff) // 2):
+                min_cost += costs[diff[i][-1]][-1]
+            else:
+                min_cost += costs[diff[i][-1]][0]
         
-        for i in range(len(diff) // 2, len(diff)):
-            cost += costs[diff[i][-1]][0]
-        
-        return cost
+        return min_cost
