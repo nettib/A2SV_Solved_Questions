76d5958d7c22a2d9924424c3fccb75d07f4587d0
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-05 00:49:56 +0300

    Add solution for Number of Islands

--- leetcode/0200-number-of-islands/0200-number-of-islands.py
+++ leetcode/0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
-        def inbound(r, c):
+        def inbound(r: int, c: int) -> bool:
             return 0 <= r < len(grid) and 0 <= c < len(grid[0])
 
-        visited = set()
-        def dfs(grid, r, c):
-            visited.add((r, c))
+        def dfs(r, c):
 
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
+                if (nr, nc) in visited or not inbound(nr, nc):
+                    continue
 
-                if inbound(nr, nc) and (nr, nc) not in visited and grid[nr][nc] != "0":
-                    dfs(grid, nr, nc)
-        
+                if grid[nr][nc] == "1":
+                    visited.add((nr, nc))
+                    dfs(nr, nc)
 
-        count = 0 
+        visited = set()
+        islands = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == "1" and (r, c) not in visited:
-                    dfs(grid, r, c)
-                    count += 1
+                    visited.add((r, c))
+                    islands += 1
+                    dfs(r, c)
 
-        return count       
+        return islands
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
