ff87784a2464c896cdf8365a8fcc834192651658
Netsanet Tibebu <netsanettibebu098@gmail.com>
2026-06-06 19:52:24 +0300

    Add solution for Course Schedule

--- leetcode/0207-course-schedule/0207-course-schedule.py
+++ leetcode/0207-course-schedule/0207-course-schedule.py
@@ -1,41 +1,42 @@
-from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
-
-        for courses in prerequisites:
-            c1, c2 = courses
-            graph[c2].append(c1)
+        # 0 = "WHITE", 1 = "GRAY", 2 = "BLACK"
         
-        colors = {}
+        courses = defaultdict(list)
+        colors = [0] * numCourses
 
-        for node in range(numCourses):
-            colors[node] = 'W'
-
+        for c1, c2 in prerequisites:
+            courses[c2].append(c1)
+        
         visited = set()
-        cycle = False
-        def dfs(node):
-            nonlocal cycle
-            visited.add(node)
-            colors[node] = 'G'
-
-            if cycle:
-                return
-
-            for neighbor in graph[node]:
-                if colors[neighbor] == 'W':
-                    dfs(neighbor)
-                elif colors[neighbor] == 'G':
-                    cycle = True
-            colors[node] = 'B'
+        def dfs(course):
+            
+            for nei in courses[course]:
+                if nei in visited:
+                    if colors[nei] == 1:
+                        return False
+                    continue
+                colors[nei] = 1
+                visited.add(nei)
+                if not dfs(nei):
+                    return False
+            colors[course] = 2
+            return True
         
-        for node in range(numCourses):
-            if node not in visited:
-                dfs(node)
+        for course in range(numCourses):
+            if course not in visited:
+                colors[course] = 1
+                visited.add(course)
+                if not dfs(course):
+                    return False
         
-        return not cycle
+        return True
         
 
-        
 
 
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
